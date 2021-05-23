9c0656b93351c34c34c479eea19a0a18ec47b241
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-23 18:50:21 -0500

    added container for about me

--- src/About.js
+++ src/About.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from 'react'
+import { Container, Divider } from 'semantic-ui-react'
 
 class About extends React.Component {
 
   render(){
     return (
       <div>
-          An energetic Full Stack Software Developer emerging from a 10+ year 
-          career in the customer service and hospitality industries. 
-          Skilled with strong interpersonal communication, adaptability, 
-          and attention to detail, allow me to collaborate with clients and 
-          cross-functional teams to quickly resolve problems. Experienced in 
-          Ruby on Rails, React, Redux, and JavaScript-based programming coupled 
-          with a degree in psychology that invokes relentless interest in enhancing 
-          the user experience. Excited to learn and grow in this ever-changing industry.   
+        <Container textAlign='left'>Left Aligned</Container>
+        <Container textAlign='center'>Center Aligned</Container>
+        <Container textAlign='right'>Right Aligned</Container>
+        <Container textAlign='justified'>
+            <b>About Me</b>
+            <Divider />
+            <p>
+                An energetic Full Stack Software Developer emerging from a 10+ year 
+                career in the customer service and hospitality industries. 
+                Skilled with strong interpersonal communication, adaptability, 
+                and attention to detail, allow me to collaborate with clients and 
+                cross-functional teams to quickly resolve problems. Experienced in 
+                Ruby on Rails, React, Redux, and JavaScript-based programming coupled 
+                with a degree in psychology that invokes relentless interest in enhancing 
+                the user experience. Excited to learn and grow in this ever-changing industry.   
+            </p>
+        </Container>
       </div>
     )
   }
