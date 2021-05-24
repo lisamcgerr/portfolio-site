4705b64b27010d2211f9afbe4e440d19f8829a32
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-24 13:53:02 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import About from './About'
 import Contact from './Contact'
+import Home from './Home'
+import ProjectContainer from './ProjectContainer'
 
 class App extends React.Component {
 
   render(){
     return (
-      <div>   
+      <div>
+        <Switch>
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/projects' component={ProjectContainer} />
+          <Route exact path='/' component={Home} />
+        </Switch>   
         <Navbar />
+        <Home />
         This is the app component
         <About />
         <Contact />
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { Image } from 'semantic-ui-react'
 
 class Home extends React.Component {
 
   render(){
     return (
       <div>  
+        <Image src='https://user-images.githubusercontent.com/73184313/119392568-873cf480-bc95-11eb-90a5-e0f19fca8e5e.png' size='medium' circular />
         background image spanning and Lisa McGerr with image and Web Developer title 
       </div>
     )
