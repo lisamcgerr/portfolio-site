aac34c203e20647adcb2bf43cf1ff112bedbc3c4
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-24 13:27:06 -0500

    Revert "revert"
    This reverts commit d7e168c0f57bafe043b0d6b2021126e30654a187.

--- src/About.js
+++ src/About.js
@@ -6,22 +6,23 @@ class About extends React.Component {
   render(){
     return (
       <div>
-        <Container textAlign='left'>Left Aligned</Container>
-        <Container textAlign='center'>Center Aligned</Container>
-        <Container textAlign='right'>Right Aligned</Container>
         <Container textAlign='justified'>
-            <b>About Me</b>
+            <h1>About Me</h1>
             <Divider />
+            <h3>
+                Let's build something great together!
+            </h3>
             <p>
-                An energetic Full Stack Software Developer emerging from a 10+ year 
+                An energetic Full-Stack Software Developer emerging from a 10+ year 
                 career in the customer service and hospitality industries. 
                 Skilled with strong interpersonal communication, adaptability, 
-                and attention to detail, allow me to collaborate with clients and 
+                and attention to detail, which allow me to collaborate with clients and 
                 cross-functional teams to quickly resolve problems. Experienced in 
                 Ruby on Rails, React, Redux, and JavaScript-based programming coupled 
                 with a degree in psychology that invokes relentless interest in enhancing 
                 the user experience. Excited to learn and grow in this ever-changing industry.   
             </p>
+            <iframe textAlign='center' width="800" height="450" src="https://www.youtube.com/embed/cJh1kPJiEp8" title="About Lisa McGerr" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </Container>
       </div>
     )
