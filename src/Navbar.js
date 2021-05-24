723e3db331915f169c024e032892505a93008259
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-24 13:58:07 -0500

    adding navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
 
 class Navbar extends React.Component {
 
   render(){
     return (
       <div>  
-          navbar 
+        <Menu className='navbar' secondary >
+            <Menu.Item
+              name='Lisa McGerr'/>
+          <Link to="/about">
+            <Menu.Item
+              name='About'/>
+          </Link>
+          <Link to="/projects">
+            <Menu.Item
+              name='Portfolio'/>
+          </Link>
+          <Link to="/contact">
+            <Menu.Item
+              name='Contact'/>
+          </Link>
+        </Menu>
       </div>
     )
   }
