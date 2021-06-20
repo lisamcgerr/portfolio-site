aed30ed6369832b428e3b708a09b3d34be2acbfc
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-06-20 12:12:03 -0500

    change css on hover for icons

--- src/SkillsContainer.js
+++ src/SkillsContainer.js
@@ -7,20 +7,24 @@ import { Grid } from 'semantic-ui-react'
 
 class SkillsContainer extends React.Component {
 
+  mouseHover = () => {
+
+  }
+
   renderSkills = () => {
     const skills = [
-      <DiRuby className='hover' style={{color: '#cc0000'}} size={150}pacity={.7}  />,  
-      <DiReact className='hover' style={{color: '#61dbfb'}} size={150}  />,
-      <SiRedux className='hover' style={{color: '#764abc'}} size={150}  />,
-      <DiJavascript1 className='hover' style={{color: '#f0db4f'}} size={150}  />, 
-      <DiRor className='hover' style={{color: '#cc0000'}} size={150}  />, 
-      <DiHtml5 className='hover' style={{color: '#f06529'}} size={150}  />, 
-      <DiSqllite className='hover' style={{color: '#3c3c5a'}} size={150}  />, 
-      <DiGithubBadge className='hover' style={{color: '#4078c0'}} size={150}  />, 
-      <DiCss3 className='hover' style={{color: '2965f1'}} size={150}  />,
-      <DiBootstrap className='hover' style={{color: '#944dff'}} size={150}  />,
-      <DiVisualstudio className='hover' style={{color: '#0078d7'}} size={150}  />, 
-      <DiTerminal className='hover' style={{color: '#4d4d4d'}} size={150} opacity={.7}  />
+      <DiRuby className='hover' style={{color: '#cc0000'}} size={150}pacity={.7} onMouseOver={this.mouseHover} />,  
+      <DiReact className='hover' style={{color: '#61dbfb'}} size={150} onMouseOver={this.mouseHover} />,
+      <SiRedux className='hover' style={{color: '#764abc'}} size={150} onMouseOver={this.mouseHover} />,
+      <DiJavascript1 className='hover' style={{color: '#f0db4f'}} size={150}  onMouseOver={this.mouseHover} />, 
+      <DiRor className='hover' style={{color: '#cc0000'}} size={150} onMouseOver={this.mouseHover}  />, 
+      <DiHtml5 className='hover' style={{color: '#f06529'}} size={150} onMouseOver={this.mouseHover} />, 
+      <DiSqllite className='hover' style={{color: '#3c3c5a'}} size={150} onMouseOver={this.mouseHover} />, 
+      <DiGithubBadge className='hover' style={{color: '#4078c0'}} size={150} onMouseOver={this.mouseHover} />, 
+      <DiCss3 className='hover' style={{color: '2965f1'}} size={150}  onMouseOver={this.mouseHover} />,
+      <DiBootstrap className='hover' style={{color: '#944dff'}} size={150} onMouseOver={this.mouseHover} />,
+      <DiVisualstudio className='hover' style={{color: '#0078d7'}} size={150} onMouseOver={this.mouseHover} />, 
+      <DiTerminal className='hover' style={{color: '#4d4d4d'}} size={150} onMouseOver={this.mouseHover} />
     ]
     return skills.map(skillIcon => (
         <SkillsCard 
