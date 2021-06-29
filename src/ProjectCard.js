b7887351566f3a7bebc70714b27ce94c75c3db85
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-06-28 22:38:03 -0500

    added text to videos

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -9,32 +9,44 @@ class ProjectCard extends React.Component {
     return (
       <> 
         <Grid item textAlign='center' xs={6}>
-        <iframe 
-          width="560" 
-          height="315" 
-          src="https://www.youtube.com/embed/WSbX3-Bpv0I" 
-          title="YouTube video player" 
-          frameborder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-          allowfullscreen>
-        </iframe>
-            <h3>Cooking with Friends</h3>
-            <p>A Ruby on Rails, React and Redux full stack app that allows users to virtually create cooking classes with other users</p> 
+          <br></br>
+            <h2>Cooking with Friends</h2>
+              <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/WSbX3-Bpv0I" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+              </iframe>
+                <strong>A Ruby on Rails, React and Redux full stack astrongp that allows users to virtually create cooking classes with other users</strong>
+                  <ul textAlign='left'>
+                    <li>Integrated JSON Web Tokens and localStorage to store encrypt and persist client-side information</li>
+                    <li>Built an API utilizing Rails Serializers to structure backend data</li>
+                    <li>Utilized Redux and React to store frontend state across components</li>
+                    <li>Incorporated Semantic UI for clean and organized styling</li>
+                  </ul> 
         </Grid>
         
         <Grid item textAlign='center' xs={6}>
-          <iframe 
-            width="560" 
-            height="315" 
-            src="https://www.youtube.com/embed/1SaYnOCLOGQ" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowfullscreen>
-          </iframe>
-
-            <h3>Whirlpool</h3>
-            <p>A Ruby on Rails app that allows users to find community around games</p> 
+          <br></br>
+            <h2>Whirlpool</h2>
+              <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/1SaYnOCLOGQ" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+              </iframe>
+                <strong>A Ruby on Rails app that allows users to find community around games</strong> 
+                  <ul textAlign='left'>
+                    <li>Utilized session cookies and Bcrypt to store user data for secure log-in </li>
+                    <li>Implemented Active Storage to enable profile picture uploads</li>
+                    <li>Installed Active Record validations to ensure the validity of data stored to database and client-side</li>
+                  </ul> 
         </Grid>
       </>
     )
